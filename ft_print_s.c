71d86b0ebc39f30a3fb7ddccd3f962f5b6143cca
Aleksija <78377289+ayaaleksija@users.noreply.github.com>
2021-10-30 14:08:20 +0200

    Add files via upload

---
+++ ft_print_s.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_s.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agondard <agondard@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/30 00:24:35 by agondard          #+#    #+#             */
+/*   Updated: 2021/10/30 14:00:36 by agondard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+void	ft_print_s(t_printf *print)
+{
+	char*	str;
+	str = va_arg(print->list, char *);
+	if (str == 0)
+		ft_putstr("(null)", print);
+	else
+		ft_putstr(str, print);
+}
